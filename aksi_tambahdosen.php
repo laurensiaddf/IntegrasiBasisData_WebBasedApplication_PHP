4d9f944d401a2db5595f9045e1d421e05564452c
Laurensia Divina Dewi Fortuna <58225801+laurensiaddf@users.noreply.github.com>
2019-11-26 23:19:43 +0700

    Add files via upload

---
+++ aksi_tambahdosen.php
@@ -0,0 +1,16 @@
+<?php
+include "koneksi.php";
+
+$nidn=$_POST['nidn'];
+$nama=$_POST['nama'];
+$alamat=$_POST['alamat'];
+$email=$_POST['email'];
+$doswal=$_POST['doswal'];
+$query=mysqli_query($connect, "insert into dosen(nidn, nama, alamat, email)
+values('$nidn','$nama','$alamat','$email')");
+if($query){
+	header('location:dosen.php');
+} else{
+	echo mysqli_error($connect);
+}
+?>
---
+++ hapus_dosen.php
@@ -0,0 +1,11 @@
+<?php
+include "koneksi.php";
+$id=$_GET['id'];
+$query=mysqli_query($connect, "delete from dosen where id_dosen='$id'")
+or die(mysqli_error($connect));
+if($query){
+	header('location:dosen.php');
+}else{
+	echo mysqli_error($connect);
+}
+?>
